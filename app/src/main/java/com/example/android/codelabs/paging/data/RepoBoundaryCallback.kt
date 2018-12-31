343d21cbbdda16dfc4616f31a44e0703a75ce269
Drew Hamilton <drew.hamilton.0@gmail.com>
2018-12-31 16:18:37 +0100

    Fully implement page handling using RepoBoundaryCallback

---
+++ app/src/main/java/com/example/android/codelabs/paging/data/RepoBoundaryCallback.kt
@@ -0,0 +1,55 @@
+package com.example.android.codelabs.paging.data
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.PagedList
+import com.example.android.codelabs.paging.api.GithubService
+import com.example.android.codelabs.paging.api.searchRepos
+import com.example.android.codelabs.paging.db.GithubLocalCache
+import com.example.android.codelabs.paging.model.Repo
+
+/**
+ * Created by Backbase R&D B.V. on 31/12/2018.
+ */
+class RepoBoundaryCallback(
+        private val query: String,
+        private val service: GithubService,
+        private val cache: GithubLocalCache
+) : PagedList.BoundaryCallback<Repo>() {
+
+    val networkErrors: LiveData<String>
+        get() = _networkErrors
+
+    private val _networkErrors = MutableLiveData<String>()
+
+    private var lastRequestedPage = 1
+
+    private var isRequestInProgress = false
+
+    override fun onZeroItemsLoaded() {
+        requestAndSaveData(query)
+    }
+
+    override fun onItemAtEndLoaded(itemAtEnd: Repo) {
+        requestAndSaveData(query)
+    }
+
+    private fun requestAndSaveData(query: String) {
+        if (isRequestInProgress) return
+
+        isRequestInProgress = true
+        searchRepos(service, query, lastRequestedPage, NETWORK_PAGE_SIZE, { repos ->
+            cache.insert(repos) {
+                lastRequestedPage++
+                isRequestInProgress = false
+            }
+        }, { error ->
+            _networkErrors.postValue(error)
+            isRequestInProgress = false
+        })
+    }
+
+    companion object {
+        private const val NETWORK_PAGE_SIZE = 50
+    }
+}
